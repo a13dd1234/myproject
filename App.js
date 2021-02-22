77d70791bf7e324174657b1c67ed127dcc96d873
a13dd1234 <53269307+a13dd1234@users.noreply.github.com>
2021-02-22 20:07:54 +0200

    fix import StyleSheet to { StyleSheet }

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 
 import styles from './AppStyles'
 
 const App = () => {
+  // console.log("xxxxxxxxxxxxxxxxxxxxxxxx")
+  // console.log(styles)
 
   const [name, setName] = useState('Ahmad')
+  const [person, setPerson] = useState({ name: 'Fatma', age: 22 })
+
   const [nameSwitch, setNameSwitch] = useState(true)
+
   let clickHandler = () => {
 
     console.log("1: ", nameSwitch)
-   setNameSwitch(!nameSwitch)
+    setNameSwitch(!nameSwitch)
     console.log("2: ", nameSwitch)
     nameSwitch ? setName('Fatma') : setName('Ahmad')
+    nameSwitch ? setPerson({ name: 'Ahmad', age: 40 }) : setPerson({ name: 'Fatma', age: 22 })
+
   }
   return (
     <View style={styles.container}>
       <Text>Hello, World!</Text>
       <Text>My name is {name}</Text>
+      <Text>My name is {person.name} and I am {person.age} years old!</Text>
+
       <View style={styles.buttonContainer}>
-        <Button title='Update State' onPress={clickHandler}/>
+        <Button title='Update State' onPress={clickHandler} />
       </View>
     </View>
   )
--- AppStyles.js
+++ AppStyles.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import StyleSheet from 'react-native'
+import { StyleSheet } from 'react-native'
 
-const AppStyles = () => {
-  return StyleSheet.create({
-
+export default StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
     },
-
     buttonContainer: {
       marginTop: 50,
     },
 
   })
-}
+
+
+// export default AppStyles
