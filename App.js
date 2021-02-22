869b7f76c387b5e92dc21b54e1d33fb96f1d25b8
a13dd1234 <53269307+a13dd1234@users.noreply.github.com>
2021-02-22 19:33:37 +0200

    Add hooks

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 import styles from './AppStyles'
 
 const App = () => {
+
+  const [name, setName] = useState('Ahmad')
+  const [nameSwitch, setNameSwitch] = useState(true)
+  let clickHandler = () => {
+
+    console.log("1: ", nameSwitch)
+   setNameSwitch(!nameSwitch)
+    console.log("2: ", nameSwitch)
+    nameSwitch ? setName('Fatma') : setName('Ahmad')
+  }
   return (
     <View style={styles.container}>
       <Text>Hello, World!</Text>
-      <Text>Have a nice day!</Text>
+      <Text>My name is {name}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Update State' onPress={clickHandler}/>
+      </View>
     </View>
   )
 }
--- AppStyles.js
+++ AppStyles.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import StyleSheet from 'react-native'
 
-// const AppStyles = () => {
-//   return StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       backgroundColor: '#fff',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//     },
-//   })
-// }
+const AppStyles = () => {
+  return StyleSheet.create({
 
-// export default AppStyles
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
 
+    buttonContainer: {
+      marginTop: 50,
+    },
 
-export default  StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+  })
+}
