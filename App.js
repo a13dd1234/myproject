c6c0540e5caa722ee4b5362e6687050fa940a5a9
a13dd1234 <53269307+a13dd1234@users.noreply.github.com>
2021-02-22 19:05:34 +0200

    npx react-native init - Clean Up

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import styles from './AppStyles'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, World!</Text>
+      <Text>Have a nice day!</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ AppStyles.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import StyleSheet from 'react-native'
+
+// const AppStyles = () => {
+//   return StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//   })
+// }
+
+// export default AppStyles
+
+
+export default  StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
